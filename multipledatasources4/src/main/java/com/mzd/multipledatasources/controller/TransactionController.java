2ad4c57df1fba2f66a057480ac9ae23b1fdcd427
zhouyue01 <zhouyue01@vcredit.com>
2021-09-08 17:25:32 +0800

    本地测试

--- multipledatasources4/src/main/java/com/mzd/multipledatasources/controller/TransactionController.java
+++ multipledatasources4/src/main/java/com/mzd/multipledatasources/controller/TransactionController.java
@@ -1,13 +1,18 @@
 package com.mzd.multipledatasources.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mzd.multipledatasources.bean.TeachersBean;
 import com.mzd.multipledatasources.bean.TestBean;
+import com.mzd.multipledatasources.mapper.TransactionMapping2;
 import com.mzd.multipledatasources.service.TransactionService1;
 import com.mzd.multipledatasources.service.TransactionService2;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -22,6 +27,10 @@ public class TransactionController {
 	private TransactionService1 ts1;
 	@Autowired
 	private TransactionService2 ts2;
+	@Autowired
+	private SqlSession sqlSession;
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@RequestMapping("/savetest")
 	public String savetest() {
@@ -35,13 +44,14 @@ public class TransactionController {
 	}
 
 	@RequestMapping("/saveteacher")
-	public String saveteacher() {
+	public String saveteacher() throws JsonProcessingException {
 		TeachersBean tb = new TeachersBean();
 		tb.setId(UUID.randomUUID().toString().replaceAll("-", ""));
 		tb.setTeachername("2");
 		tb.setClassid("11");
-		ts2.test02_saveTeachersBean(tb);
-		return "success";
+		//ts2.test02_saveTeachersBean(tb);
+		List<TeachersBean> list = sqlSession.getMapper(TransactionMapping2.class).select();
+		return objectMapper.writeValueAsString(list);
 	}
 
 	
--- multipledatasources4/src/main/java/com/mzd/multipledatasources/mapper/TransactionMapping2.java
+++ multipledatasources4/src/main/java/com/mzd/multipledatasources/mapper/TransactionMapping2.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Repository;
 
 import com.mzd.multipledatasources.bean.TeachersBean;
 
+import java.util.List;
+
 @Repository
 public interface TransactionMapping2 {
 
 	void save(TeachersBean t);
+
+	List<TeachersBean> select();
 
 }
